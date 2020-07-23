b327bc9eb6de414d0bfb2c95e75095075264cccc
Neelesh Maharjan <neeleshmrz@gmail.com>
2020-07-23 16:58:20 +0545

    Updates

---
+++ precache-manifest.d462784e31f17b59f46ea463ae5262da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "386e07ffcba11625c05787b1785f8f36",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "714611811384303efcf9",
+    "url": "/portfolio/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "714611811384303efcf9",
+    "url": "/portfolio/static/js/main.1994f260.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
